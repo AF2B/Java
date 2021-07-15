4797ebf03ceac2ce3e1beef1ccdca5029dc52a6f
André Filipe <81587964+pluma01@users.noreply.github.com>
2021-07-15 11:02:31 -0300

    Add files via upload
    POO

---
+++ calculadorajava/Metódos.java
@@ -0,0 +1,24 @@
+package calculadorajava;
+
+public class Metódos {
+
+	double soma(double num1, double num2) {
+		double resultado = num1 + num2;
+		return resultado;
+	}
+
+	double subtracao(double num1, double num2) {
+		double resultado = num1 - num2;
+		return resultado;
+	}
+
+	double divisao(double num1, double num2) {
+		double resultado = num1 / num2;
+		return resultado;
+	}
+
+	double multiplicacao(double num1, double num2) {
+		double resultado = num1 * num2;
+		return resultado;
+	}
+}
